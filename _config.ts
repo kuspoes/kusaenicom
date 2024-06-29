e27afc5bd21bde9dbefa7c97840f5a277d2bfe94
Kusaeni <kusaeni@gmail.com>
2024-06-30 06:36:51 +0700

    deploy env

--- _config.ts
+++ _config.ts
@@ -12,6 +12,7 @@ import title from "https://deno.land/x/lume_markdown_plugins@v0.7.0/title.ts";
 import minifyHTML from "lume/plugins/minify_html.ts";
 import sitemap from "lume/plugins/sitemap.ts";
 import inline from "lume/plugins/inline.ts";
+import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
 
 const markdown = {
   plugins: [footnotes],
@@ -142,5 +143,7 @@ site.process([".html"], (pages) => {
     }
   }
 });
+
+const token = Deno.env.get("GTS_TOKEN");
 
 export default site;
